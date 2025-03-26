46bc3f9b72c22c46b3951fc286aba8da40e9af90
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-26 20:30:24 -0300

    feat: add project on repository

---
+++ src/main/java/com/dio/bradesco_dev_week/BradescoDevWeekApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bradesco_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bradesco_dev_week/BradescoDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bradesco_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
