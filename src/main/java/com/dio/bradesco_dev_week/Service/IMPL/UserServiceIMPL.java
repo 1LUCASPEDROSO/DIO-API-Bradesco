d6f049d5e5fb3a3c7de7f974114106027bdc3c4b
lucas pedroso <pedrosolucas1745@gmail.com>
2025-04-30 17:59:04 -0300

    feat: create userService implementation based in interface with inital methods for find user and create user

---
+++ src/main/java/com/dio/bradesco_dev_week/Service/IMPL/UserServiceIMPL.java
@@ -0,0 +1,29 @@
+package com.dio.bradesco_dev_week.Service.IMPL;
+
+import com.dio.bradesco_dev_week.Domain.Entity.User;
+import com.dio.bradesco_dev_week.Domain.Repository.UserRepository;
+import com.dio.bradesco_dev_week.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceIMPL implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userCreate) {
+        if(userRepository.existsByAccountNumber(userCreate.getAccount().getNumber())){
+            throw  new RuntimeException("This Account Number already exists");
+        }
+        return userRepository.save(userCreate);
+    }
+}
