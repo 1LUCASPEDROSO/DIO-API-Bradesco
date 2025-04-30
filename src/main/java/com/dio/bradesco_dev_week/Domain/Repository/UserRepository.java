7333d7317a0bacce9ab4f9fea082fb3f444f9717
lucas pedroso <pedrosolucas1745@gmail.com>
2025-04-30 17:59:38 -0300

    feat: create new query in repository to search by accountNumber of user

--- src/main/java/com/dio/bradesco_dev_week/Domain/Repository/UserRepository.java
+++ src/main/java/com/dio/bradesco_dev_week/Domain/Repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.dio.bradesco_dev_week.Domain.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    Boolean existsByAccountNumber(String accountNumber);
 }
